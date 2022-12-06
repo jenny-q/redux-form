c294a8394a841f67de44b4ed80d485842fa1432d
Jennifer Quispe <jenny.q@me.com>
2022-12-06 10:56:29 -0500

    updated form, car list, + bulma, custom styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import 'bulma/css/bulma.css'
+import './styles.css'
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import {store} from './store'
+
 const el = document.getElementById('root');
 const root = createRoot(el);
 
